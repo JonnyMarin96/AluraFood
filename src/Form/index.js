2b3e7012a4e54faae94428c8992bcac63c297b11
JonnyMarin96 <jonny.yair.2@gmail.com>
2023-09-22 16:47:37 -0600

    Se agrego info

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Typography } from "@mui/material";
 import { LogoSpace, MainSpace, FormSpace, Img } from "./styles";
 import DatosUsuario from "./DatosUsuario";
@@ -6,16 +6,92 @@ import DatosPersonales from "./DatosPersonales";
 import DatosEntrega from "./DatosEntrega";
 import Complete from "./Complete";
 import Stepper from "../Stepper";
+import { validarEmail, validarPassword } from "./DatosUsuario/validaciones ";
+import { validarNombre, validarApellido, validarTelefono } from "./DatosPersonales/validaciones";
+import Step from "./Step";
 
 const Form = () => {
-  const [ step, setStep ] = useState(2);
-
+  const [ step, setStep ] = useState(0);
+  const [ pasos, setpasos ] = useState({})
 
   const steps = {
     0: <DatosUsuario/>,
     1: <DatosPersonales/>,
     2: <DatosEntrega/>,
     3: <Complete/>
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    let newStep = step + 1
+    setStep(newStep)
+    console.log("NewStep: ",newStep)
+    console.log("Step: ",step)
+  }
+
+  const handleChange = (e, position, currentStep, validator) => {
+    const value = e.target.value
+    const valid = validator(value)
+  }
+
+  const stepFlow = {
+    0: {
+      inputs: [
+        { /* Email */
+          label: "Correo electrónico",
+          type: "email",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperText: "Ingresa un correo electrónico válido",
+          validator: validarEmail
+        },
+        { /* Contraseña */
+        label: "Contraseña",
+        type: "Contraseña",
+        value: "",
+        valid: null,
+        onChange: handleChange,
+        helperText: "Contraseña invalida",
+        validator: validarPassword
+      }
+      ],
+      buttonText: "Siguiente",
+      onSubmit
+    },
+    1: {
+      inputs: [
+        { /* Nombre */
+          label: "Nombre",
+          type: "text",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperText: "Se permiten entre 2 y 30 caracteres",
+          validator: validarNombre,
+        },
+        { /* Apellido */         
+          label: "Apellidos",
+          type: "text",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperText: "Se permiten entre 2 y 50 caracteres",
+          validator: validarApellido
+        },
+        { /* Telefono */
+          label: "Número telefónico",
+          type: "number",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperText: "Se permiten entre 8 y 14 digitos",
+          validator: validarTelefono
+        }
+      ],
+      buttonText: "Siguiente",
+      onSubmit
+    }
   }
 
   return (
@@ -32,7 +108,8 @@ const Form = () => {
       </LogoSpace>
       <FormSpace>
         { step < 3 && <Stepper step={step}/> }  
-        { steps[step] }
+        {/* steps[step]*/ }
+        <Step data={ stepFlow[step] } step={step}/> 
       </FormSpace>
     </Box>
   );
